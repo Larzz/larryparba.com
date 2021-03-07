fed1b3bf70eb797dda5e0896f0357f251f3f4c68
Larry Parba <larz.parba@gmail.com>
2021-03-07 17:44:26 +0400

    initial files for larryparba.com

---
+++ src/Main/index.js
@@ -0,0 +1,32 @@
+
+function App() {
+  return (
+    <div className="App">
+          <div id="wrapper">
+            <div id="bg"></div>
+            <div id="overlay"></div>
+            <div id="main">
+                <header id="header">
+                  <h1>Larry Parba</h1>
+                  <p>Security Chief &nbsp;&bull;&nbsp; Cyborg &nbsp;&bull;&nbsp; Never asked for this</p>
+                  <nav>
+                    <ul>
+                      <li><a href="#" className="icon brands fa-twitter"><span className="label">Twitter</span></a></li>
+                      <li><a href="#" className="icon brands fa-facebook-f"><span className="label">Facebook</span></a></li>
+                      <li><a href="#" className="icon brands fa-dribbble"><span className="label">Dribbble</span></a></li>
+                      <li><a href="#" className="icon brands fa-github"><span className="label">Github</span></a></li>
+                      <li><a href="#" className="icon solid fa-envelope"><span className="label">Email</span></a></li>
+                    </ul>
+                  </nav>
+                </header>
+                <footer id="footer">
+                  <span className="copyright">&copy; Untitled. Design: <a href="http://html5up.net">HTML5 UP</a>.</span>
+                </footer>
+
+            </div>
+          </div>
+      </div>
+  );
+}
+
+export default App;
