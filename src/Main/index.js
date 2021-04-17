93d57904b07e0997b729a4ca331ae33cf4491f57
Larry <larry@creativouae.com>
2021-04-17 22:57:18 +0400

    added

--- src/Main/index.js
+++ src/Main/index.js
@@ -8,14 +8,12 @@ function App() {
             <div id="main">
                 <header id="header">
                   <h1>Larry Parba</h1>
-                  <p>Security Chief &nbsp;&bull;&nbsp; Cyborg &nbsp;&bull;&nbsp; Never asked for this</p>
+                  <p>Web Application Developer &nbsp;&bull;&nbsp; System Admin &nbsp;&bull;&nbsp; Server Management</p>
                   <nav>
                     <ul>
-                      <li><a href="#" className="icon brands fa-twitter"><span className="label">Twitter</span></a></li>
-                      <li><a href="#" className="icon brands fa-facebook-f"><span className="label">Facebook</span></a></li>
-                      <li><a href="#" className="icon brands fa-dribbble"><span className="label">Dribbble</span></a></li>
-                      <li><a href="#" className="icon brands fa-github"><span className="label">Github</span></a></li>
-                      <li><a href="#" className="icon solid fa-envelope"><span className="label">Email</span></a></li>
+                      <li><a href="https://www.instagram.com/larry.parba/" className="icon brands fa-instagram"><span className="label">Instagram</span></a></li>
+                      <li><a href="https://github.com/Larzz" className="icon brands fa-github"><span className="label">Github</span></a></li>
+                      <li><a href="mailto:larz.parba@gmail.com" className="icon solid fa-envelope"><span className="label">Email</span></a></li>
                     </ul>
                   </nav>
                 </header>
