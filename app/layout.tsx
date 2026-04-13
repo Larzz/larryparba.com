4391b165df44ae7bcf82a2722be10627872b8326
Larry Parba <larz.parba@gmail.com>
2026-04-13 23:59:03 +0800

    feat: add addsense

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import AdSense from 'react-adsense';
 
 const inter = Inter({
 	variable: '--font-inter',
@@ -19,6 +20,14 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang='en' className={`${inter.variable} h-full antialiased`}>
+			<script async src="https://www.googletagmanager.com/gtag/js?id=G-WKBDP55SD0"></script>
+			<script>
+			window.dataLayer = window.dataLayer || [];
+			function gtag(){dataLayer.push(arguments);}
+			gtag('js', new Date());
+
+			gtag('config', 'G-WKBDP55SD0');
+			</script>
 			<body className='min-h-full flex flex-col bg-zinc-50'>{children}</body>
 		</html>
 	)
